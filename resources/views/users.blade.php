bf1ceccaa57ed43e967e1c227d861a3efae5f8a0
Gavin Moulton <gavin.moulton@gmail.com>
2020-01-10 14:45:24 +0000

    change color

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,11 +1,11 @@
     <div class="content">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                        <table bgcolor="#ff6600"><tr><td>id</td><td>name</td><td>role</td><td>email</td><td>company_name</td><td>registered_on</td><td>last_login</td></tr>
+                        <table><tr><td>id</td><td>name</td><td>role</td><td>email</td><td>company_name</td><td>registered_on</td><td>last_login</td></tr>
                         @foreach($results as $result)
                                 <tr>
                                     <td>{{$result->id}}</td>
-                                    <td>{{$result->name}}</td>
+                                    <td><h1>{{$result->name}}</h1></td>
                                     <td>{{$result->role}}</td>
                                     <td>{{$result->email}}</td>
                                     <td>{{$result->company_name}}</td>
